fcb476392828a809bffcfcdc63e957fc74649eba
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-10-19 21:12:40 +0100

    Fixed equation for posterior distribution

--- O17-86/mcmc_normal.R
+++ O17-86/mcmc_normal.R
@@ -27,10 +27,38 @@ mean(mcmc_package)
 # Use the same test data as for MCMCpack
 plot(x, D, type='l', ylab="p(X)", xlab="x")
 
-# Define the prior distribution
+# Define the prior distribution (mean=0, variance=1)
 mean_prior <- dnorm(x, 0, 1)
 
 # Define the posterior distribution
-mean_posterior <- (D + mean_prior)
-plot(x, mean_posterior, type="l")
+mean_posterior <- (D * mean_prior)
+
+plot(x, mean_posterior, type="l", ylim=c(0,0.6))
+par(new=TRUE)
+plot(x, mean_prior, type="l", col="red", ylim=c(0,0.6))
+
+# Apply the Metropolis algorithm
+
+
+# https://theoreticalecology.wordpress.com/2010/09/17/metropolis-hastings-mcmc-in-r/
+# proposalfunction <- function(param){
+#   return(rnorm(3,mean = param, sd= c(0.1,0.5,0.3)))
+# }
+# run_metropolis_MCMC <- function(startvalue, iterations){
+#   chain = array(dim = c(iterations+1,3))
+#   chain[1,] = startvalue
+#   for (i in 1:iterations){
+#     proposal = proposalfunction(chain[i,])
+#     
+#     probab = exp(posterior(proposal) - posterior(chain[i,]))
+#     if (runif(1) < probab){
+#       chain[i+1,] = proposal
+#     }else{
+#       chain[i+1,] = chain[i,]
+#     }
+#   }
+#   return(chain)
+# }
+
+
 
