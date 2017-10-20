d05892a2c3f8ab4d0dab01cb261b7afdf9e72e30
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-10-20 14:37:06 +0100

    changed likelihood function

--- mcmc_normal.R
+++ mcmc_normal.R
@@ -7,15 +7,31 @@
 # Packages needed
 library("MCMCpack")
 
+################# 
+## Create data ##
+#################
+
+obs_var <- 1
+obs_sd <- obs_var^(1/2)
+obs_mean <- 2
+
+prior_var <- 1
+prior_mean <- 0
+
+# create independent x-values 
+x <- seq(-2, 6, 0.01)
+# create dependent values according to Normal distribution
+D <- dnorm(x, obs_mean, obs_var)
+
+plot(x,D, main="Test Data")
+
 ####################################################### 
 ## Use MCMCpack as a comparison for my manual method ##
 #######################################################
 
 # The data, known variance of data, prior mean of mu, prior variance of mu, number of MC draws
 # don't know mean of data!
-x <- seq(-2, 6, 0.01)
-D <- dnorm(x, 2, 1) # A vector of observed data
-mcmc_package <- MCnormalnormal(D, 1, 0, 1, mc = 1000)
+mcmc_package <- MCnormalnormal(D, obs_var, prior_mean, prior_var, mc = 1000)
 plot(mcmc_package)
 var(mcmc_package)
 mean(mcmc_package)
@@ -27,7 +43,15 @@ mean(mcmc_package)
 # Use the same test data as for MCMCpack
 # This may be wrong
 plot(x, D, type='l', ylab="p(X)", xlab="x")
-likelihood <- sum(D)
+likelihood <- function(param){
+  mean = param[1]
+  var = param[2]
+  
+  pred = dnorm(x, obs_mean, obs_var, log=T)
+  singlelikelihoods = dnorm(D, mean = pred, obs_var, log = T)
+  sumll = sum(singlelikelihoods)
+  return(sumll)   
+}
 
 # Define the prior distribution (mean=0, variance=1)
 mean_prior <- dnorm(x, 0, 1)
@@ -43,9 +67,24 @@ plot(x, mean_prior, type="l", col="red", ylim=c(0,45))
 # Start at random parameter value
 # Choose new parameter close to old value based on prob. density ratio
 # Jump to new probability if P(old)/P(new) >1
-
+metropolis=function(n=1000,var=1){
+  vec=vector("numeric", n)
+  x=set.seed[1]
+  vec[1]=x
+  for (i in 2:n) {
+    innov=runif(1,-eps,eps)
+    can=x+innov
+    aprob=min(1,dnorm(can)/dnorm(x))
+    u=runif(1)
+    if (u < aprob)
+      x=can
+    vec[i]=x
+  }
+  vec
+}
 
 # https://theoreticalecology.wordpress.com/2010/09/17/metropolis-hastings-mcmc-in-r/
+# This example works in logs!
 # proposalfunction <- function(param){
 #   return(rnorm(3,mean = param, sd= c(0.1,0.5,0.3)))
 # }
