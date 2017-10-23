62b7137832e147d26ce413afdd8a01ebf21a81f0
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-10-23 11:39:59 +0100

    An attempt at the Metropolis algorithm

--- mcmc_normal.R
+++ mcmc_normal.R
@@ -43,24 +43,70 @@ mean(mcmc_package)
 
 # Use the same test data as for MCMCpack
 # This may be wrong
-likelihood <- dnorm(x, obs_mean, obs_sd, log= T)
+likelihood <- function(param){
+  dnorm(x, obs_mean, obs_sd, log= T)  
+} 
+
 
 # Define the prior distribution (mean=0, variance=1)
-mean_prior <- dnorm(x, prior_mean, prior_sd, log=T)
+# mean_prior <- dnorm(x, prior_mean, prior_sd, log=T)
+mean_prior <- function(param){
+  a = param[1]
+  aprior = dnorm(x, prior_mean, prior_sd, log = T)
+  return(aprior)
+}
 
 # Define the posterior distribution
 # Note: is on a logarithmic scale
-mean_posterior <- likelihood + mean_prior
+#mean_posterior <- likelihood + mean_prior
+mean_posterior <- function(param){
+  return (likelihood(param) + mean_prior(param))
+}
 
-plot(x, mean_posterior, type="l", col = "red", ylim = c(-30, 0), ylab = "log(mean)")
-par(new=TRUE)
-plot(x, mean_prior, type="l", col="black", ylim = c(-30, 0), ylab = " ")
+# plot(x, mean_posterior, type="l", col = "red", ylim = c(-30, 0), ylab = "log(mean)")
+# par(new=TRUE)
+# plot(x, mean_prior, type="l", col="black", ylim = c(-30, 0), ylab = " ")
 
 # Apply the Metropolis algorithm
+# Probability density distribution for proposing new values
+#proposalfunction <- (rnorm(1, mean_prior, prior_sd))
+
+proposalfunction <- function(param){
+  return(rnorm(1,mean = param, prior_sd))
+}
+
 # Start at random parameter value
 # Choose new parameter close to old value based on prob. density ratio
 # Jump to new probability if P(old)/P(new) >1
 
+metropolis_MCMC <- function(startvalue, iterations){
+  chain = array(dim = c(iterations+1))
+  chain[1] = startvalue
+  for (i in 1:iterations){
+    proposal = proposalfunction(chain[i])
+
+    probab = exp(mean_posterior(proposal) - mean_posterior(chain[i]))
+    if (runif(1) < probab){
+      chain[i+1] = proposal
+    }else{
+      chain[i+1] = chain[i]
+    }
+  }
+  return(chain)
+}
+
+startvalue = prior_mean
+chain = metropolis_MCMC(startvalue, 10000)
+
+burnIn = 5000
+acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
+
+par(mfrow = c(1,2))
+hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of mean", xlab="True value = red line" )
+abline(v = mean(chain[-(1:burnIn),1]))
+#abline(v = trueA, col="red" )
+plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of mean")
+#abline(h = trueA, col="red" )
 
 # https://theoreticalecology.wordpress.com/2010/09/17/metropolis-hastings-mcmc-in-r/
 # This example works in logs!
@@ -82,23 +128,5 @@ plot(x, mean_prior, type="l", col="black", ylim = c(-30, 0), ylab = " ")
 #   }
 #   return(chain)
 # }
-
-# metropolis=function(n=1000,eps=5) 
-# {
-#   vec=vector("numeric", n)
-#   x=0
-#   vec[1]=x
-#   for (i in 2:n) {
-#     innov=runif(1,-eps,eps)
-#     can=x+innov
-#     aprob=min(1,dnorm(can)/dnorm(x))
-#     u=runif(1)
-#     if (u < aprob) 
-#       x=can
-#     vec[i]=x
-#   }
-#   vec
-# }
-
 
 
