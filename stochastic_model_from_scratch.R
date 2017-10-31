cdcb2322a64a54aa7d10f7638c50bb5967239bc8
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-10-31 09:49:19 +0000

    Runs and produces a good-looking plot but population size is not stable

--- stochastic_model_from_scratch.R
+++ stochastic_model_from_scratch.R
@@ -26,6 +26,10 @@ D_inf <- 2
 data <- array(0, dim =c(length(times), length(init.values)+1))
 data[,1] <- times # make first column the timesteps so you know what's happening when making plots
 
+# Calculating probabilities that do not change with time
+p <- R0 * (timestep/(D_inf*N)) # probability of effective contact
+r_t <- (1/D_inf)*timestep # Recovery rate
+
 # For loops for calculating each individual's disease status at each timepoint
 for (time in times){
   if (time == 0){ # Set up the number of S/I/R at time 0
@@ -34,56 +38,26 @@ for (time in times){
     data[1,4] <- init.values["R"] # number of recovereds at time 0
     
   } else{
-    data[time+1,2] <- 1 # number of susceptibles at other times
-    data[time+1,3] <- 1 # number of infecteds at other times
-    data[time+1,4] <- 1 # number of recovereds at other times
+    foi_t <- 1 - (1 - p)^data[time, 3] # take data on the number infectious from previous timestep!
+    
+    data[time+1,2] <- data[time, 2] - rbinom(1, size = data[time,2], prob = foi_t) # number of susceptibles at other times
+    
+    data[time+1,3] <- data[time, 3]  + rbinom(1, size = data[time,2], prob = foi_t) - rbinom(1, size = data[time,3], prob = r_t) # number of infecteds at other times
+    
+    data[time+1,4] <- data[time,4] + rbinom(1, size = data[time,3], prob = r_t) # number of recovereds at other times
   }
 }
 
-######################################################################################################################
+run_stoch <- data.frame(data)
 
-# Array for holding people's disease statuses within a timepoint
-# status <- array(0, dim = c(N,1))
-  
-# # For loops for calculating each individual's disease status at each timepoint
-# for (time in times){
-#   if (time < 1){ # Set up the number of infecteds and susceptibles at time 0
-#     status[1:init.values["I"]] <- "I"
-#     status[init.values["I"]+1:N] <- "S"
-#   }
-#   if (time > 0){ # All other timepoints depend on the previous timepoint
-#     status[1:N] <-"R" # debugging
-#     for (ind in 1:N){
-# 
-#     # If individual's status at ind is R, they stay R
-#     if (status[ind] == "R"){
-#       status[ind] <- "R" # they stay recovered
-#     }
-# 
-#     # If status at ind is I:
-#     } else if (status[ind] == "I"){
-# 
-#     r_t <- (1/D_inf)*timestep # Recovery rate
-#     x <- rbinom(1, N, r_t)
-#       if (x < r_t){
-#         status[ind] <- "R"
-#       } else {
-#         status[ind] <- "I"
-#       }
-#     } else { # if status[ind] == "S"
-#       # Force of infection at time t
-#       p <- R0 * (timestep/(D_inf*init.values["N"])) # probability of effective contact
-#       foi_t <- 1 - (1 - p)^data[time-1, 2] # take data on the infectious from prev timestep!
-#       # risk of individual becoming infected in next time interval
-#       x <- runif(1, 0.0, 1.0) # choose a random number between 0 and 1
-#       if (x < foi_t){ # if x < foi_t, individual becomes infected
-#         status[ind] <- "I"
-#       } else { # else individual remains susceptible
-#         status[ind] <- "S"
-#       }
-#   }}
-# # Sum up  S/I/R from the status array and add them on into the data array
-# data[time, 1] <- length(status[status=="S"]) # sum of susceptibles for the timepoint
-# data[time, 2] <- length(status[status=="I"]) # sum of infecteds for the timepoint
-# data[time, 3] <- length(status[status=="R"]) # sum of recovereds for the timepoint
-# }
+plot(x = run_stoch$X1, y = run_stoch$X3, type = "line", col = "red", ylim = c(0,10e3),
+     xlab = "Time", ylab = "Number susceptible/infected/recovered", main = "Stochastic SIR Model")
+par(new=T)
+plot(x = run_stoch$X1, y = run_stoch$X2, type = "line", ylim = c(0,10e3), ylab = "", xlab = "") # add susceptible line
+par(new=T)
+plot(x = run_stoch$X1, y = run_stoch$X4, type = "line", col = "orange", ylim = c(0,10e3), ylab = "", xlab = "") # recovered
+
+## Add legend
+legend(80, 8000, c("Susceptible", "Infected", "Recovered"), pch = 1, col = c("black", "red", "orange"), bty = "n")
+
+######################################################################################################################
