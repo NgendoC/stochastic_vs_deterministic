e9905a978abcefb5fb8ed3325290b65d72bb3608
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-10-31 15:47:09 +0000

    starting over, it runs something now

--- mcmc_two_unknown.R
+++ mcmc_two_unknown.R
@@ -1,84 +1,49 @@
 ## MCMC with two unknowns
 ## 31/10/2017
 
-################ 
-## Known data ##
-################
+trueA <- 5
+trueSd <- 10
+sampleSize <- 31
 
-# Data observation
-#D <- 2 # 1 observation
-D <- rnorm(30, mean = 2, sd = 1)  # many observations, random numbers from a normal distribution
+# create values according to a + N(0,sd)
+y <- rnorm(n=sampleSize,mean=trueA,sd=trueSd)
 
-# Number of observations
-num_obs <- length(D)
-
-# Mean and variance for the prior distribution 
-prior_var <- 1
-prior_sd <- sqrt(prior_var)
-prior_mean <- 0
-
-# Proposal variance
-proposal_var <- 1
-proposal_sd <- sqrt(proposal_var)
-
-#################
-## Manual MCMC ##
-#################
-
-# Likelihood function for the observation
 likelihood <- function(param){
-  mean = param[1]
-  var = param[2]
+  a = param[1]
+  sd = param[2]
   
-  obs_likelihood = dnorm(D, mean, sqrt(var), log = T)
-  sum_likelihood = sum(obs_likelihood)
-  return(sum_likelihood)
-} 
-
-# Define the prior distribution
-prior <- function(param){
-  mean = param[1]
-  var = param[2]
-  
-  mean_prior = dnorm(mean, mean = 0, sd = 1, log = T)
-  var_prior = dnorm(var, mean = 0, sd = 1, log = T)
-  return(mean_prior + var_prior)
+  singlelikelihoods = dnorm(y, mean = a, sd = sd, log = T)
+  sumll = sum(singlelikelihoods)
+  return(sumll)   
 }
 
-# Define the posterior distribution
-# Note: is on a logarithmic scale
+# Prior distribution
+prior <- function(param){
+  a = param[1]
+  sd = param[2]
+  aprior = dnorm(a, mean = 0, sd = 1, log = T)
+  sdprior = dnorm(sd, mean = 0, sd = 1, log = T)
+  return(aprior+sdprior)
+}
+
 posterior <- function(param){
   return (likelihood(param) + prior(param))
 }
 
-# Apply the Metropolis algorithm
+######## Metropolis algorithm ################
 
-# Probability density distribution for proposing new values
-# Proposal function has a Normal distribution 
-# The distribution is centered around the current value in the Markov chain
 proposalfunction <- function(param){
-  return(rnorm(2, param, sd = c(1,1)))
+  return(rnorm(2,mean = param, sd= c(0.1,0.3)))
 }
 
-# Start at random parameter value
-# Choose new parameter close to old value based on prob. density ratio
-
-metropolis_MCMC <- function(startvalue, iterations){
+run_metropolis_MCMC <- function(startvalue, iterations){
   chain = array(dim = c(iterations+1,2))
-  
-  # Give a starting point to the chain
   chain[1,] = startvalue
-  
-  # For the other iterations, use the proposal function to suggest the next value
   for (i in 1:iterations){
     proposal = proposalfunction(chain[i,])
     
-    # P(new)/P(old) NOTE: LOGARITHMIC
-    prob = exp(posterior(proposal) - posterior(chain[i,]))
-    
-    # Jump to new probability if P(new)/P(old) >1
-    # if r < than P, accept, else reject
-    if (runif(1) < prob){ 
+    probab = exp(posterior(proposal) - posterior(chain[i,]))
+    if (runif(1) < probab){
       chain[i+1,] = proposal
     }else{
       chain[i+1,] = chain[i,]
@@ -87,29 +52,139 @@ metropolis_MCMC <- function(startvalue, iterations){
   return(chain)
 }
 
-# Where to start the chain
-# Takes a random number from the (Normal) prior distribution
-startvalue <- c(1,1) #rnorm(2, prior_mean, prior_sd)
+startvalue = c(4,10)
+chain = run_metropolis_MCMC(startvalue, 10000)
 
-# Number of runs
-iterations = 10000
-set.seed(4)
-chain <- metropolis_MCMC(startvalue, iterations)
+burnIn = 5000
+acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
 
-# The beginning of the chain is biased towards the starting point, so take them out
-# normally burnin is 10%-50% of the runs
-burnIn = 0.1*iterations
-acceptance <- 1-mean(duplicated(chain[-(1:burnIn)]))
+par(mfrow = c(2,2))
+hist(chain[-(1:burnIn),1],nclass=30, , main="Posterior of a", xlab="True value = red line" )
+abline(v = mean(chain[-(1:burnIn),1]))
+abline(v = trueA, col="red" )
 
-###########
-## Plots ##
-###########
+hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of sd", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),2]))
+abline(v = trueSd, col="red" )
 
-# Plot the MCMC
-par(mfrow = c(1,2))
-hist(chain[-(1:burnIn)],nclass=30, main="Posterior mean", freq=FALSE, xlim=c(-2,4), ylim=c(0,1.0), col = "grey")
-abline(v = mean(chain[-(1:burnIn)]))
-plot(chain[-(1:burnIn)], type = "l", main = "Chain values of mean")
+plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a", )
+abline(h = trueA, col="red" )
+
+plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of sd", )
+abline(h = trueSd, col="red" )
+
+###################################################################################################################
+## Previous attempt
+# 
+# ################ 
+# ## Known data ##
+# ################
+# 
+# # Data observation
+# #D <- 2 # 1 observation
+# D <- rnorm(30, mean = 2, sd = 1)  # many observations, random numbers from a normal distribution
+# 
+# # Number of observations
+# num_obs <- length(D)
+# 
+# # Mean and variance for the prior distribution 
+# prior_var <- 1
+# prior_sd <- sqrt(prior_var)
+# prior_mean <- 0
+# 
+# # Proposal variance
+# proposal_var <- 1
+# proposal_sd <- sqrt(proposal_var)
+# 
+# #################
+# ## Manual MCMC ##
+# #################
+# 
+# # Likelihood function for the observation
+# likelihood <- function(param){
+#   mean = param[1]
+#   var = param[2]
+#   
+#   obs_likelihood = dnorm(D, mean, sqrt(var), log = T)
+#   sum_likelihood = sum(obs_likelihood)
+#   return(sum_likelihood)
+# } 
+# 
+# # Define the prior distribution
+# prior <- function(param){
+#   mean = param[1]
+#   var = param[2]
+#   
+#   mean_prior = dnorm(mean, mean = 0, sd = 1, log = T)
+#   var_prior = dnorm(var, mean = 0, sd = 1, log = T)
+#   return(mean_prior + var_prior)
+# }
+# 
+# # Define the posterior distribution
+# # Note: is on a logarithmic scale
+# posterior <- function(param){
+#   return (likelihood(param) + prior(param))
+# }
+# 
+# # Apply the Metropolis algorithm
+# 
+# # Probability density distribution for proposing new values
+# # Proposal function has a Normal distribution 
+# # The distribution is centered around the current value in the Markov chain
+# proposalfunction <- function(param){
+#   return(rnorm(2, param, sd = c(1,1)))
+# }
+# 
+# # Start at random parameter value
+# # Choose new parameter close to old value based on prob. density ratio
+# 
+# metropolis_MCMC <- function(startvalue, iterations){
+#   chain = array(dim = c(iterations+1,2))
+#   
+#   # Give a starting point to the chain
+#   chain[1,] = startvalue
+#   
+#   # For the other iterations, use the proposal function to suggest the next value
+#   for (i in 1:iterations){
+#     proposal = proposalfunction(chain[i,])
+#     
+#     # P(new)/P(old) NOTE: LOGARITHMIC
+#     prob = exp(posterior(proposal) - posterior(chain[i,]))
+#     
+#     # Jump to new probability if P(new)/P(old) >1
+#     # if r < than P, accept, else reject
+#     if (runif(1) < prob){ 
+#       chain[i+1,] = proposal
+#     }else{
+#       chain[i+1,] = chain[i,]
+#     }
+#   }
+#   return(chain)
+# }
+# 
+# # Where to start the chain
+# # Takes a random number from the (Normal) prior distribution
+# startvalue <- c(1,1) #rnorm(2, prior_mean, prior_sd)
+# 
+# # Number of runs
+# iterations = 10000
+# set.seed(4)
+# chain <- metropolis_MCMC(startvalue, iterations)
+# 
+# # The beginning of the chain is biased towards the starting point, so take them out
+# # normally burnin is 10%-50% of the runs
+# burnIn = 0.1*iterations
+# acceptance <- 1-mean(duplicated(chain[-(1:burnIn)]))
+# 
+# ###########
+# ## Plots ##
+# ###########
+# 
+# # Plot the MCMC
+# par(mfrow = c(1,2))
+# hist(chain[-(1:burnIn)],nclass=30, main="Posterior mean", freq=FALSE, xlim=c(-2,4), ylim=c(0,1.0), col = "grey")
+# abline(v = mean(chain[-(1:burnIn)]))
+# plot(chain[-(1:burnIn)], type = "l", main = "Chain values of mean")
 
 
 # Helpful resource for MCMC in R:
