a6eb08eae11b7cd8e3cb173a48bc5e72389ca113
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2017-11-14 11:59:21 +0000

    rewrote SIR model in terms of beta and gamma

--- stochastic_with_mcmc.R
+++ stochastic_with_mcmc.R
@@ -17,9 +17,9 @@ init.values = c(
 )
 N = sum(init.values)
 
-# Invent some epidemic data to fit my model to
-true_infectious <- (dnorm(times, 20, 4))*N
-#plot(true_infectious)
+# Beta & gammma
+beta <- 0.008
+gamma <- 0.5
 
 ###############
 ## The model ##
@@ -28,14 +28,6 @@ true_infectious <- (dnorm(times, 20, 4))*N
 # Array for holding collective disease status information for whole period of time
 data <- array(0, dim =c(length(times), length(init.values)+3))
 data[,1] <- times # make first column the timesteps to make plotting easier later on
-  
-# R0 & duration of infectiousness
-R0 <- 3
-D_inf <- 2
-  
-# Calculating probabilities that do not change with time
-p <- R0 * (timestep/(D_inf*N)) # probability of effective contact
-r_t <- (1/D_inf)*timestep # Recovery rate
   
 # For loops for calculating the numbers susceptible, infected, and recovered at each timepoint
 for (time in times){
@@ -48,9 +40,11 @@ for (time in times){
       
   } else{
     whole_time <- 1/timestep * time # makes time into the whole number that it corresponds to in the array
-    foi_t <- 1 - (1 - p)^data[whole_time, 3] # Force of infection at time t, affected by the number infectious at prev t
-    inf <- rbinom(1, size = data[whole_time,2], prob = foi_t) # number who become infected in this timestep
-    rec <- rbinom(1, size = data[whole_time,3], prob = r_t)# number who become recovered in this timestep
+    #inf <- rbinom(1, size = data[whole_time,2], prob = foi_t) # number who become infected in this timestep
+    #rec <- rbinom(1, size = data[whole_time,3], prob = r_t)# number who become recovered in this timestep
+    
+    inf <- rbinom(1, size = data[whole_time,2], (1-(exp(-beta*data[whole_time,3]*timestep)))) # number who become infected in this timestep
+    rec <- rbinom(1, size = data[whole_time,3], (1-(exp(-gamma*timestep)))) # number who become recovered in this timestep
       
     data[whole_time+1,2] <- data[whole_time,2] - inf # number of susceptibles at other times
       
@@ -98,7 +92,7 @@ likelihood <- function(param){
   beta = as.numeric(param[1])
   gamma = as.numeric(param[2])
   total = 0
-  
+
   for (i in 1:nrow(run_stoch)){
   betalikelihood = dbinom(run_stoch$new_I, run_stoch$S, (1-(exp(-beta*run_stoch$I[i-1]*timestep))), log = T)
   gammalikelihood = dbinom(run_stoch$new_R, run_stoch$I, (1-(exp(-gamma*timestep))), log = T)
@@ -111,7 +105,7 @@ likelihood <- function(param){
 prior <- function(param){
   beta = as.numeric(param[1])
   gamma = as.numeric(param[2])
-  
+
   betaprior = dunif(beta, min = 0, max = 100, log = T)
   gammaprior = dunif(gamma, min = 0, max = 100, log = T)
   return(betaprior + gammaprior)
@@ -123,12 +117,12 @@ posterior <- function(param){
 }
 
 proposalfunction <- function(param){ # beta and gamma need to be >0
-  
+
   beta_prop = rnorm(1, mean = param[1], sd = 1)
   gamma_prop = rnorm(1, mean = param[2], sd = 1)
   #beta_prop = runif(1, min = 0, max = 10)
   #gamma_prop = runif(1, min = 0, max = 10)
-  
+
   #print(c(beta_prop, gamma_prop))
   return(c(beta_prop, gamma_prop))
 }
@@ -138,7 +132,7 @@ run_metropolis_MCMC <- function(startvalue, iterations){
   chain[1,] = startvalue
   for (i in 1:iterations){
     proposal = proposalfunction(chain[i,])
-    
+
     # if (proposal[1] < 0.0 | proposal[2] < 0.0){
     #   chain[i+1,] = chain[i,]
     # }
@@ -186,3 +180,38 @@ plot(chain[-(1:burnIn),2], type = "l", main = "Chain values of gamma")
 ########################################################################################################################
 
 ########################################################################################################################
+
+# # R0 & duration of infectiousness
+# R0 <- 3
+# D_inf <- 2
+# 
+# # Calculating probabilities that do not change with time
+# p <- R0 * (timestep/(D_inf*N)) # probability of effective contact
+# r_t <- (1/D_inf)*timestep # Recovery rate
+# 
+# # For loops for calculating the numbers susceptible, infected, and recovered at each timepoint
+# for (time in times){
+#   if (time == 0){ # Set up the number of S/I/R at time 0
+#     data[1,2] <- init.values["S"] # number of susceptibles at time 0
+#     data[1,3] <- init.values["I"] # number of infecteds at time 0
+#     data[1,4] <- init.values["R"] # number of recovereds at time 0
+#     data[1,5] <- init.values["I"] # number newly infected at time 0
+#     data[1,6] <- init.values["R"] # number newly recovered at time 0
+#     
+#   } else{
+#     whole_time <- 1/timestep * time # makes time into the whole number that it corresponds to in the array
+#     foi_t <- 1 - (1 - p)^data[whole_time, 3] # Force of infection at time t, affected by the number infectious at prev t
+#     inf <- rbinom(1, size = data[whole_time,2], prob = foi_t) # number who become infected in this timestep
+#     rec <- rbinom(1, size = data[whole_time,3], prob = r_t)# number who become recovered in this timestep
+#     
+#     data[whole_time+1,2] <- data[whole_time,2] - inf # number of susceptibles at other times
+#     
+#     data[whole_time+1,3] <- data[whole_time,3]  + inf - rec # number of infecteds at other times
+#     
+#     data[whole_time+1,4] <- data[whole_time,4] + rec # number of recovereds at other times
+#     
+#     data[whole_time+1,5] <- data[whole_time+1,3] - data[whole_time,3] + data[whole_time+1,4] - data[whole_time,4] # number of newly infected
+#     
+#     data[whole_time+1,6] <- data[whole_time+1,4] - data[whole_time,4] # number of newly recovered
+#   }
+# }
