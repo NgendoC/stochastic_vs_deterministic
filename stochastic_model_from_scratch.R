508fab92345859f7d6b0d7d702ae2a31e0154ef1
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2018-01-29 17:28:44 +0000

    Made stochastic model into what the different parameterisation methods use. Added csv output

--- stochastic_model_from_scratch.R
+++ stochastic_model_from_scratch.R
@@ -6,8 +6,9 @@
 ##########################
 
 # Time
-timestep <- 0.1
-times <- seq(0, 100, by = timestep)
+timestep <- 0.5
+end <- 80
+times <- seq(0, end, by = timestep)
 
 # Initial population: N-1 susceptible, 1 infectious, 0 recovered
 init.values = c(
@@ -17,21 +18,19 @@ init.values = c(
 )
 N = sum(init.values)
 
-# R0 & duration of infectiousness
-R0 <- 3
-D_inf <- 2
+# Beta & gammma
+beta <- 5e-3
+gamma <- 8e-2
 
 ###############
 ## The model ##
 ###############
 
 # Array for holding collective disease status information for whole period of time
-data <- array(0, dim =c(length(times), length(init.values)+1))
+data <- array(0, dim =c(length(times), length(init.values)+3))
 data[,1] <- times # make first column the timesteps to make plotting easier later on
 
-# Calculating probabilities that do not change with time
-p <- R0 * (timestep/(D_inf*N)) # probability of effective contact
-r_t <- (1/D_inf)*timestep # Recovery rate
+set.seed(7) # Good ones: 7, 14, 22, 30
 
 # For loops for calculating the numbers susceptible, infected, and recovered at each timepoint
 for (time in times){
@@ -39,18 +38,24 @@ for (time in times){
     data[1,2] <- init.values["S"] # number of susceptibles at time 0
     data[1,3] <- init.values["I"] # number of infecteds at time 0
     data[1,4] <- init.values["R"] # number of recovereds at time 0
+    data[1,5] <- init.values["I"] # number newly infected at time 0
+    data[1,6] <- init.values["R"] # number newly recovered at time 0
     
   } else{
     whole_time <- 1/timestep * time # makes time into the whole number that it corresponds to in the array
-    foi_t <- 1 - (1 - p)^data[whole_time, 3] # Force of infection at time t, affected by the number infectious at prev t
-    inf <- rbinom(1, size = data[whole_time,2], prob = foi_t) # number who become infected in this timestep
-    rec <- rbinom(1, size = data[whole_time,3], prob = r_t)# number who become recovered in this timestep
     
-    data[whole_time+1,2] <- data[whole_time, 2] - inf # number of susceptibles at other times
+    inf <- rbinom(1, size = data[whole_time,2], (1-(exp(-beta*data[whole_time,3]*timestep)))) # number who become infected in this timestep
+    rec <- rbinom(1, size = data[whole_time,3], (1-(exp(-gamma*timestep)))) # number who become recovered in this timestep
     
-    data[whole_time+1,3] <- data[whole_time, 3]  + inf - rec # number of infecteds at other times
+    data[whole_time+1,2] <- data[whole_time,2] - inf # number of susceptibles at other times
+    
+    data[whole_time+1,3] <- data[whole_time,3]  + inf - rec # number of infecteds at other times
     
     data[whole_time+1,4] <- data[whole_time,4] + rec # number of recovereds at other times
+    
+    data[whole_time+1,5] <- data[whole_time+1,3] - data[whole_time,3] + data[whole_time+1,4] - data[whole_time,4] # number of newly infected
+    
+    data[whole_time+1,6] <- data[whole_time+1,4] - data[whole_time,4] # number of newly recovered
   }
 }
 
@@ -59,18 +64,26 @@ for (time in times){
 ###############
 
 run_stoch <- data.frame(data) # make array into a dataframe
-colnames(run_stoch) <- c("time","S", "I", "R")
+colnames(run_stoch) <- c("time","S", "I", "R", "new_I", "new_R")
 
-#par(mfrow = c(2,2))
+par(mfrow = c(1,1))
 
 # Plot for SIR model
-plot(x = run_stoch$time, y = run_stoch$I, type = "line", col = "red", ylim = c(0,N),
+plot(x = run_stoch$time, y = run_stoch$I, type = "l", col = "red", ylim = c(0,N),
      xlab = "Time", ylab = "Number susceptible/infected/recovered", main = "Stochastic SIR Model")
 par(new=T)
-plot(x = run_stoch$time, y = run_stoch$S, type = "line", ylim = c(0,N), ylab = "", xlab = "") # add susceptible line
+plot(x = run_stoch$time, y = run_stoch$S, type = "l", ylim = c(0,N), ylab = "", xlab = "") # add susceptible line
 par(new=T)
-plot(x = run_stoch$time, y = run_stoch$R, type = "line", col = "orange", ylim = c(0,N), ylab = "", xlab = "") # recovered
+plot(x = run_stoch$time, y = run_stoch$R, type = "l", col = "orange", ylim = c(0,N), ylab = "", xlab = "") # recovered
 
-## Add legend
-legend(40, 0.8*N, c("Susceptible", "Infected", "Recovered"), pch = 1, col = c("black", "red", "orange"), bty = "n")
+# Add legend
+legend(60, 0.8*N, c("Susceptible", "Infected", "Recovered"), pch = 1, col = c("black", "red", "orange"), bty = "n")
 
+###############
+## Save data ##
+###############
+
+setwd("C:/Users/Janetta Skarp/OneDrive - Imperial College London/MRes_BMR/Project_1/Work_folder/Data")
+
+# Save SIR data 
+write.csv(run_stoch, file = "run_stoch.csv", row.names = FALSE)
