55f24f5c7636efb1fda02ad10de05147b48f2d9d
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2018-01-30 11:40:15 +0000

    Reorganised to make modifications easier

--- residual_error_2par.R
+++ residual_error_2par.R
@@ -15,6 +15,16 @@ library("deSolve") #package for solving differential equations
 setwd("C:/Users/Janetta Skarp/OneDrive - Imperial College London/MRes_BMR/Project_1/Work_folder/Data")
 run_stoch <- read.csv("run_stoch.csv")
 
+###########
+## Input ##
+###########
+
+# Starting point for parameters
+beta = 0.005
+gamma = 0.08
+
+iterations = 10 # how many runs for bootstraps
+
 #########################
 ## Deterministic model ##
 #########################
@@ -91,8 +101,8 @@ sse_bootstrap <- function(param){
 
 # Starting point for parameters
 param <- c(
-  1, # 5e-3, # beta
-  1# 8e-2 # gamma
+  beta, # beta
+  gamma # gamma
 )
 
 # Optimisation function
@@ -104,8 +114,6 @@ rownames(sse_point_data) <- c("point")
 ############################
 ## Bootstrap optimisation ##
 ############################
-
-iterations = 20 # how many runs for bootstraps
 
 # Dataset for storing iteration data
 sse_bootstrap_data = array(dim = (c(iterations, length(param)+1)))
@@ -126,31 +134,30 @@ for (i in 1:iterations){
 ## Plots ##
 ###########
 
-# Histogram
-par(mfrow = c(1,2))
+# # Histogram
+# par(mfrow = c(1,2))
+# 
+# hist(sse_bootstrap_data[,1],nclass=30, main="Beta", xlab="Beta value")
+# abline(v = sse_fit$par[1], col = "red")
+# 
+# hist(sse_bootstrap_data[,2],nclass=30, main="Gamma", xlab="Gamma value")
+# abline(v = sse_fit$par[2], col = "red")
+# 
+# 
+# # Lines
+# run_det <- as.data.frame(ode(y = init.values, times = times, func = sir, parms = sse_fit$par))
+# 
+# par(mfrow = c(1,1))
+# plot(run_stoch$R, ylim = c(0, N), type = "l", col = "orange", xlab = "Timestep", ylab = "Number of individuals")
+# lines(run_det$I, type = "l", col = "red", xlab = " ", ylab = " ")
+# lines(run_stoch$I, type = "l", col = "grey", xlab = " ", ylab = " ")
+# lines(run_det$R, type = "l", col = "black", xlab = "", ylab = "")
+# legend(100, 0.5*N, c("Deterministic recovered", "True recovered", "Deterministic infected", "True infected"), pch = 1, col = c("black", "orange", "red", "grey"), bty = "n")
+# 
+# # Beta vs. Gamma
+# par(mfrow = c(1,1))
+# plot(x = sse_bootstrap_data[,2], y = sse_bootstrap_data[,1], xlab = "Gamma", ylab = "Beta", pch = 20, cex = 0.8)
 
-hist(sse_bootstrap_data[,1],nclass=30, main="Beta", xlab="Beta value")
-abline(v = sse_fit$par[1], col = "red")
-
-hist(sse_bootstrap_data[,2],nclass=30, main="Gamma", xlab="Gamma value")
-abline(v = sse_fit$par[2], col = "red")
-
-
-# Lines
-run_det <- as.data.frame(ode(y = init.values, times = times, func = sir, parms = sse_fit$par))
-
-par(mfrow = c(1,1))
-plot(run_stoch$R, ylim = c(0, N), type = "l", col = "orange", xlab = "Timestep", ylab = "Number of individuals")
-lines(run_det$I, type = "l", col = "red", xlab = " ", ylab = " ")
-lines(run_stoch$I, type = "l", col = "grey", xlab = " ", ylab = " ")
-lines(run_det$R, type = "l", col = "black", xlab = "", ylab = "")
-legend(100, 0.5*N, c("Deterministic recovered", "True recovered", "Deterministic infected", "True infected"), pch = 1, col = c("black", "orange", "red", "grey"), bty = "n")
-
-# Beta vs. Gamma
-par(mfrow = c(1,1))
-plot(x = sse_bootstrap_data[,2], y = sse_bootstrap_data[,1], xlab = "Gamma", ylab = "Beta", pch = 20, cex = 0.8)
-
-sse_data <- rbind(sse_point_data, sse_bootstrap_data)
 ########################################################################################################################
 
 ########################################################################################################################
@@ -158,6 +165,5 @@ sse_data <- rbind(sse_point_data, sse_bootstrap_data)
 setwd("C:/Users/Janetta Skarp/OneDrive - Imperial College London/MRes_BMR/Project_1/Work_folder/Data")
 
 # Beta, gamma, and residual error data
+sse_data <- rbind(sse_point_data, sse_bootstrap_data)
 write.csv(data.frame(sse_data), file = "re_betagamma_test.csv", row.names = FALSE) # point estimate, bootstrap and residual error data
-
-check_data <- read.csv("re_betagamma_test.csv")
