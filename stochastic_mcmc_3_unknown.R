3382ca0c8e19f0bd16a55fd630ea22f7d889d768
Janetta Evelina Skarp <janetta.skarp13@imperial.ac.uk>
2018-01-30 11:54:52 +0000

    Deleted plots, now in plots from data file

--- stochastic_mcmc_3_unknown.R
+++ stochastic_mcmc_3_unknown.R
@@ -12,8 +12,6 @@ run_stoch <- read.csv("run_stoch.csv")
 ## Input ##
 ###########
 
-inf_period <- 10 # Guess an infectious period for infectious curve starting point
-
 # Guesses for beta and gamma
 beta = 0.005
 gamma = 0.08
@@ -21,6 +19,9 @@ gamma = 0.08
 # Proposal function SDs
 prop_sd_beta = 0.005
 prop_sd_gamma = 0.1
+
+# inf_period <- 10 # Guess an infectious period for infectious curve starting point
+inf_period <- ceiling(1/gamma) # mean infectious period calculated from gamma
 
 # Number of runs
 iterations = 5 # How many iterations MCMC is running for
@@ -34,7 +35,6 @@ N = run_stoch$S[1] + run_stoch$I[1] + run_stoch$R[1]
 
 timestep <- run_stoch$time[2] - run_stoch$time[1]
 
-# inf_period <- ceiling(1/gamma) # mean infectious period calculated from gamma
 inf_timestep <- inf_period/timestep # translates infectious days into no. of timesteps
 epi_time <- max(run_stoch$time) + inf_period
 times <- seq(0, epi_time, by = timestep)
@@ -58,13 +58,6 @@ for (i in 1:nrow(run_stoch)){
 }
 
 run_stoch[is.na(run_stoch)] <- 0
-
-plot(run_stoch$time, run_stoch$R, ylim = c(0,N), type = "l", col = "orange", xlab = "time (days)", ylab = "Number infectious/recovered")
-par(new=T)
-plot(run_stoch$time, run_stoch$guess_I, ylim = c(0,N), type = "l", col = "red", xlab = " ", ylab = " ")
-par(new=T)
-plot(x = run_stoch$time, y = run_stoch$I, type = "l", col = "black", ylim = c(0,N), xlab = " ", ylab = " ")
-legend(60, 0.8*N, c("Recovered", "Guessed infected", "True infected"), pch = 1, col = c("orange", "red", "black"), bty = "n")
 
 ##################################
 ## Likelihood, prior, posterior ##
@@ -245,7 +238,6 @@ run_metropolis_MCMC <- function(startvalue, iterations){
       temp_chain[,2,2:3] = temp_chain[,1,2:3]
     } else{
       inf_probab = inf_posterior(inf_proposal) - inf_posterior(temp_chain[,,])
-      # print(c(inf_posterior(inf_proposal), inf_posterior(temp_chain[,,])))
       if (log(runif(1)) < inf_probab){
         temp_chain[,2,2:3] = inf_proposal[,1,2:3]
       } else{
@@ -265,13 +257,7 @@ run_metropolis_MCMC <- function(startvalue, iterations){
     # Print every nth iteration, to know how far along run is
     if (i%%(iterations/20) == 0 | i == 1) {
       print(i)
-      
-      # plot(run_stoch$guess_I, ylim = c(0, N), type = "l", col = "red", xlab = "Timestep", ylab = "Number of individuals infected")
-      # lines(run_stoch$I, type = "l", col = "grey", xlab = " ", ylab = " ")
-      # lines(temp_chain[,1,2], type = "l", lty = 2, col = "black", xlab = " ", ylab = " ")
-      # legend(130, 1.0*N, c("True infected", "Guessed infected", "MCMC"), pch = 1, col = c("grey", "red", "black"), bty = "n")
     }
-    
   }
   return(chain)
 }
@@ -286,54 +272,6 @@ startvalue[,3] <- run_stoch$guess_new_I # new I guess
 # Run the MCMC
 set.seed(4)
 chain <- run_metropolis_MCMC(startvalue, iterations)
-
-# The beginning of the chain is biased towards the starting point, so take them out
-# normally burnin is 10%-50% of the runs
-# burnIn = 0.1*(iterations/divisor)
-# acceptance <- 1-mean(duplicated(chain[,-(1:burnIn),]))
-# inf_acceptance <- 1-mean(duplicated(chain[,-(1:burnIn),2]))
-
-################
-## MCMC Plots ##
-################
-
-# par(mfrow = c(2,2))
-# 
-# hist(chain[1,-(1:burnIn),1],nclass=30, main="Posterior of beta")
-# abline(v = mean(chain[1,-(1:burnIn),1]), col = "red")
-# 
-# hist(chain[2, -(1:burnIn),1],nclass=30, main="Posterior of gamma")
-# abline(v = mean(chain[2,-(1:burnIn),1]), col = "red")
-# 
-# plot(chain[1, -(1:burnIn),1], type = "l", main = "Chain values of beta")
-# 
-# plot(chain[2, -(1:burnIn),1], type = "l", main = "Chain values of gamma")
-# 
-# # Plot beta vs. gamma
-# par(mfrow = c(1,1))
-# library(RColorBrewer)
-# library(MASS)
-# 
-# plot(x = chain[2,,1], y = chain[1,,1], xlab = "Gamma", ylab = "Beta", pch = 20, cex = 0.8)
-# 
-# k <- 11
-# my.cols <- rev(brewer.pal(k, "RdYlBu"))
-# z <- kde2d(chain[2,,1], chain[1,,1], n=50)
-# filled.contour(z, nlevels=k, col=my.cols, xlab = "Gamma", ylab = "Beta")
-#  
-# par(mfrow = c(1,1))
-#   
-# plot(run_stoch$guess_I, ylim = c(0, N), type = "l", col = "red", xlab = "Timestep", ylab = "Number of individuals infected")
-#   lines(run_stoch$I, type = "l", col = "grey", xlab = " ", ylab = " ")
-#   lines(chain[,ncol(chain),2], type = "l", lty = 2, col = "black", xlab = " ", ylab = " ")
-#   legend(130, 1.0*N, c("True infected", "Guessed infected", "MCMC"), pch = 1, col = c("grey", "red", "black"), bty = "n")
-# 
-# plot(run_stoch$guess_I, ylim = c(0, N), type = "l", col = "red", xlab = "Timestep", ylab = "Number of individuals infected")
-#   lines(run_stoch$I, type = "l", col = "grey", xlab = " ", ylab = " ")  
-#   for (i in 1:ncol(chain)){
-#     lines(chain[,i,2], type = "l", lty = 2, col = "black", xlab = " ", ylab = " ")
-#   }
-#   legend(130, 1.0*N, c("True infected", "Guessed infected", "MCMC"), pch = 1, col = c("grey", "red", "black"), bty = "n")  
 
 ########################################################################################################################
 
